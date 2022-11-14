0e9bd24e45a2a3b73e19e9282e95502a2c7bd339
filiplouk <94610898+filiplouk@users.noreply.github.com>
2022-11-14 11:15:08 +0200

    Personal Site

---
+++ index.js
@@ -0,0 +1,5 @@
+// window.addEventListener('scroll', function() {
+//   const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+//   const percentage = 100*window.pageYOffset/vh;
+//   document.querySelector('.chapter a').innerHTML = percentage +"%";
+// });
