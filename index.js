651f48e60a19fca402499ad7b884a1a57f7468ca
filiplouk <94610898+filiplouk@users.noreply.github.com>
2022-11-15 11:10:07 +0200

    first try

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-// window.addEventListener('scroll', function() {
-//   const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-//   const percentage = 100*window.pageYOffset/vh;
-//   document.querySelector('.chapter a').innerHTML = percentage +"%";
-// });
+
+document.querySelector(".chapter a").style.textDecoration = "underline"
+window.addEventListener('scroll', function() {
+  const vh =  window.innerHeight;
+  const percentage = window.pageYOffset;
+  // document.querySelector('.chapter a').innerHTML = percentage +"%";
+  const chapters = document.querySelectorAll(".chapter a");
+  if (percentage<700 ){
+    chapters.forEach(function(element){
+      element.style.textDecoration = "none";
+    });
+    chapters[0].style.textDecoration = "underline";
+  }
+  else if (percentage<1250){
+    chapters.forEach(function(element){
+      element.style.textDecoration = "none";
+    });
+    chapters[1].style.textDecoration = "underline";
+  }
+  else if (percentage<3300){
+    chapters.forEach(function(element){
+      element.style.textDecoration = "none";
+    });
+    chapters[2].style.textDecoration = "underline";
+  }
+  else if (percentage<4800){
+    chapters.forEach(function(element){
+      element.style.textDecoration = "none";
+    });
+    chapters[3].style.textDecoration = "underline";
+  }
+  else{
+    chapters.forEach(function(element){
+      element.style.textDecoration = "none";
+    });
+    chapters[4].style.textDecoration = "underline";
+  }
+
+
+
+
+});
