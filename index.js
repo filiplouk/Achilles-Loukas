1333e57150eb832a0eed37adb4a7c0124363f6a7
filiplouk <94610898+filiplouk@users.noreply.github.com>
2022-11-20 11:36:14 +0200

    change main picture

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
+// -----------------  Underline Section when scroll ------------------------
+
 document.querySelector(".chapter a").style.textDecoration = "underline"
 window.addEventListener('scroll', function() {
   const vh =  window.innerHeight;
   const percentage = window.pageYOffset;
   // document.querySelector('.chapter a').innerHTML = percentage +"%";
   const chapters = document.querySelectorAll(".chapter a");
-  if (percentage<700 ){
+  if (percentage<600 ){
     chapters.forEach(function(element){
       element.style.textDecoration = "none";
     });
@@ -23,7 +25,7 @@ window.addEventListener('scroll', function() {
     });
     chapters[2].style.textDecoration = "underline";
   }
-  else if (percentage<4800){
+  else if (percentage<4300){
     chapters.forEach(function(element){
       element.style.textDecoration = "none";
     });
@@ -36,7 +38,22 @@ window.addEventListener('scroll', function() {
     chapters[4].style.textDecoration = "underline";
   }
 
-
-
-
 });
+
+
+// ------------------ Label 2 Typing -----------------------
+let txt = "I'm a Developer";
+let i = 0;
+let speed = 150;
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("label_2").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
+
+// ----------------------Enable Popovers-----------------------------
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
